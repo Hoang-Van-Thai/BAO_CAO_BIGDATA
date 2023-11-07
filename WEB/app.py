5fb7b2bfa61a563e2551e3bc7081d005cacc72c3
Hoang-Van-Thai <hoangvanthai763@gmail.com>
2023-11-07 14:47:00 +0700

    Upload

--- WEB/app.py
+++ WEB/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template, jsonify
 from pyspark.sql import SparkSession
 from pyspark.ml.recommendation import ALSModel
+from pyspark.sql.functions import col
+from pyspark.sql.functions import explode
 
 app = Flask(__name__)
 
@@ -17,6 +19,11 @@ spark = SparkSession.builder \
 # Load mô hình ALS từ HDFS
 model_path = "hdfs://localhost:9000/Models/modelASL"
 als_model = ALSModel.load(model_path)
+# Đọc dữ liệu từ tệp CSV và tạo DataFrame trong Spark
+movies_df = spark.read.csv("D:/datamovies/4_2015/movies.csv", header=True, inferSchema=True)
+
+# Đổi tên cột 'movieId' thành 'movie_id' để phù hợp với cột trong ALS recommendations
+movies_df = movies_df.withColumnRenamed("movieId", "movie_id")
 
 @app.route('/', methods=['GET'])
 def index():
@@ -30,12 +37,29 @@ def recommend():
 
         # Thực hiện dự đoán bằng mô hình ALS
         recommendations = als_model.recommendForUserSubset(spark.createDataFrame([(user_id,)], ['userId']), 10)
-        movie_ids = [r.movieId for r in recommendations.first().recommendations]
 
-        # Hiển thị user_id và movie_ids đề xuất
-        return render_template('recommendations.html', user_id=user_id, movie_ids=movie_ids)
+        # Chuyển đổi cấu trúc cột recommendations để tách thành hai cột: movieId và rating
+        recommendations = recommendations.withColumn("recommendation", explode(col("recommendations"))) \
+            .select("userId", col("recommendation.movieId").alias("movie_id"), col("recommendation.rating").alias("rating"))
+
+        # Thực hiện phép join và lấy thông tin về tiêu đề và thể loại của các bộ phim được đề xuất
+        joined_df = recommendations.join(movies_df, "movie_id") \
+            .select("userId", "movie_id", "title", "genres")
+
+        # Lấy thông tin về movie_id, title và thể loại của các bộ phim được đề xuất từ DataFrame đã join
+        movie_info = []
+        for row in joined_df.collect():
+            movie_id = row['movie_id']
+            title = row['title']
+            genres = row['genres']
+            movie_info.append({'movie_id': movie_id, 'title': title, 'genres': genres})
+
+        # Hiển thị user_id và thông tin về các bộ phim được đề xuất
+        return render_template('recommendations.html', user_id=user_id, movie_info=movie_info)
     except Exception as e:
         return jsonify({'error': str(e)})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
